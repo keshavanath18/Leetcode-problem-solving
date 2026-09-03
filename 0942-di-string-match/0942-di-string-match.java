9e24b9089d02c0032bb622a3c66964698d453306
keshavanath18 <keshavav573@gmail.com>
2026-09-03 22:04:15 +0530

    Time: 1 ms (100%), Space: 47 MB (84.14%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.java
+++ 0942-di-string-match/0942-di-string-match.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] diStringMatch(String s) {
-         
         char[] ch = s.toCharArray();
-
         int n = ch.length;
         int[] ans = new int[n + 1];
-
         int low = 0;
         int high = n;
-
         for (int i = 0; i < n; i++) {
-
             if (ch[i] == 'I') {
                 ans[i] = low;
                 low++;
@@ -19,9 +14,7 @@ class Solution {
                 high--;
             }
         }
-
         ans[n] = low;
-
         return ans;
     }
 }
