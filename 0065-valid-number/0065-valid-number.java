e49d7c3c51faedc3ffa27bb225e707b00493b173
keshavanath18 <keshavav573@gmail.com>
2026-09-08 14:29:17 +0530

    Time: 1 ms (100%), Space: 44.2 MB (20.09%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,6 +1,21 @@
 class Solution {
     public boolean isNumber(String s) {
-        s=s.trim();
-        return s.matches("[+-]?((\\d+(\\.\\d*)?)|(\\.\\d+))([eE][+-]?\\d+)?");
+        s = s.trim();
+        if(s.length()==0) return false;
+        boolean num=false,dot=false,e=false;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(Character.isDigit(c)) num=true;
+            else if(c=='.'){
+                if(dot||e) return false;
+                dot=true;
+            } else if(c=='e'||c=='E'){
+                if(e||!num) return false;
+                e=true; num=false;
+            } else if(c=='+'||c=='-'){
+                if(i!=0&&s.charAt(i-1)!='e'&&s.charAt(i-1)!='E') return false;
+            } else return false;
+        }
+        return num;
     }
 }
