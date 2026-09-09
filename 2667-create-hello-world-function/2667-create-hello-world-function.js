c6e855f9fce956beb89adc074e6fb34f25a49a66
keshavanath18 <keshavav573@gmail.com>
2026-09-09 22:58:57 +0530

    Time: 36 ms (86.04%), Space: 53.2 MB (71.67%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,5 +1,15 @@
+/**
+ * @return {Function}
+ */
 var createHelloWorld = function() {
-return function(...args){
-return "Hello World";
- };
+   
+    return function(...args) {
+        return "Hello World";
+    }
 };
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
