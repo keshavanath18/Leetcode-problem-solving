36ea68e8f253700079cda96c0f983e0de693be77
keshavanath18 <keshavav573@gmail.com>
2026-09-10 13:35:18 +0530

    Time: 6 ms (36.99%), Space: 46.4 MB (15.93%) - LeetHub

--- 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.java
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.java
@@ -6,27 +6,34 @@ class Solution {
     //         start = start.replace("RX", "XR");
     //     }
     // return start.equals(result);
-    if(!start.replace("X","").equals(end.replace("X","")))
-       return false;
-    int pointer1=0,pointer2=0;
-    while(pointer1<start.length()&&pointer2<end.length()){
-    while(pointer1<start.length()&&start.charAt(pointer1)=='X')
-        pointer1++;
-    while(pointer2<end.length()&&end.charAt(pointer2)=='X')
-        pointer2++;
-    if(pointer1==start.length()&&pointer2==end.length())
+
+
+        if (!start.replace("X", "").equals(end.replace("X", ""))) {
+            return false;
+        }
+        int i = 0, j = 0;
+        int n = start.length();
+        while (i < n && j < n) {
+            while (i < n && start.charAt(i) == 'X') i++;
+            while (j < n && end.charAt(j) == 'X') j++;
+            if (i == n || j == n) {
+                return i == n && j == n;
+            }
+            char c = start.charAt(i);
+            if (c != end.charAt(j)) {
+                return false;
+            }
+            // L can move only left
+            if (c == 'L' && i < j) {
+                return false;
+            }
+            // R can move only right
+            if (c == 'R' && i > j) {
+                return false;
+            }
+            i++;
+            j++;
+        }
         return true;
-    if(pointer1==start.length()||pointer2==end.length())
-        return false;
-    if(start.charAt(pointer1)!=end.charAt(pointer2))
-        return false;
-    if(start.charAt(pointer1)=='L'&&pointer2>pointer1)
-        return false;
-    if(end.charAt(pointer2)=='R'&&pointer1>pointer2)
-        return false;
-    pointer1++;
-    pointer2++;
-}
-return true;
     }
 }
