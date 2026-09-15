3a31ff04c7091ee789177b5c241e18b2b4edb4b8
keshavanath18 <keshavav573@gmail.com>
2026-09-15 13:07:12 +0530

    Time: 1 ms (99.17%), Space: 42.8 MB (99.64%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.java
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -1,20 +1,39 @@
 class Solution {
     public String capitalizeTitle(String title) {
-        String words[]=title.toLowerCase().split(" ");
-        String res="";
-        for(int i=0;i<words.length;i++){
-            String word=words[i];
-            if(word.length()<=2){
-                res+=word;
+    //     String words[]=title.toLowerCase().split(" ");
+    //     String res="";
+    //     for(int i=0;i<words.length;i++){
+    //         String word=words[i];
+    //         if(word.length()<=2){
+    //             res+=word;
+    //         }
+    //         else{
+    //             res+=Character.toUpperCase(word.charAt(0));
+    //             res+=word.substring(1);
+    //         }
+    //         if(i<words.length-1){
+    //             res+=" ";
+    //         }
+    //     }
+    // return res;
+    char[] arr=title.toCharArray();
+    int start=0;
+    for(int i=0;i<=arr.length;i++){
+        if(i==arr.length||arr[i]==' '){
+        int len=i-start;
+            for(int j=start;j<i;j++){
+                if(arr[j]>='A'&&arr[j]<='Z'){
+                    arr[j]=(char)(arr[j]+32);
+                }
             }
-            else{
-                res+=Character.toUpperCase(word.charAt(0));
-                res+=word.substring(1);
-            }
-            if(i<words.length-1){
-                res+=" ";
+            if(len>=3){
+            if(arr[start]>='a'&&arr[start]<='z'){
+                arr[start]=(char)(arr[start]-32);
             }
         }
-    return res;
+        start=i+1;
+        }
+    }
+    return new String(arr);
     }
 }
