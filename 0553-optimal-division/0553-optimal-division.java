49edaa4d4a77ca28e4f580fb795323f2a2b0a4a8
keshavanath18 <keshavav573@gmail.com>
2026-09-20 16:53:26 +0530

    Time: 1 ms (99.33%), Space: 43 MB (53.02%) - LeetHub

--- 0553-optimal-division/0553-optimal-division.java
+++ 0553-optimal-division/0553-optimal-division.java
@@ -1,19 +1,32 @@
 class Solution {
     public String optimalDivision(int[] nums) {
-        if(nums.length==1){
-            return ""+nums[0];
+    //     if(nums.length==1){
+    //         return ""+nums[0];
+    //     }
+    //      if (nums.length == 2) {
+    //         return nums[0] + "/" + nums[1];
+    //     }
+    //     String res=nums[0]+"/(";
+    //     for(int i=1;i<nums.length;i++){
+    //         res+=nums[i];
+    //         if(i!=nums.length-1){
+    //                 res+="/";
+    //             }
+    //     }
+    //     res+=")";
+    // return res;
+    StringBuffer sb=new StringBuffer();
+    sb.append(nums[0]);
+    for(int i=1;i<nums.length;i++){
+        sb.append("/");
+        if(i==1 &&nums.length>2){
+            sb.append("(");
         }
-         if (nums.length == 2) {
-            return nums[0] + "/" + nums[1];
+        sb.append(nums[i]);
+        if(i==nums.length-1&&nums.length>2){
+            sb.append(")");
         }
-        String res=nums[0]+"/(";
-        for(int i=1;i<nums.length;i++){
-            res+=nums[i];
-            if(i!=nums.length-1){
-                    res+="/";
-                }
-        }
-        res+=")";
-    return res;
+    }
+    return sb.toString();
     }
 }
