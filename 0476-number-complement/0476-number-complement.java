30f8c665265f2b504c88e8cd49a8f64ad2cffa72
keshavanath18 <keshavav573@gmail.com>
2026-09-21 15:49:09 +0530

    Time: 1 ms (15.86%), Space: 42.3 MB (37.99%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,16 +1,28 @@
 class Solution {
     public int findComplement(int num) {
         String bin=Integer.toBinaryString(num);
-        String res="";
+        // String res="";
+        // for(int i=0;i<bin.length();i++){
+        //     char ch=bin.charAt(i);
+        //     if(ch=='1'){
+        //         res+='0';
+        //     }
+        //     else if(ch=='0'){
+        //         res+='1';
+        //     }
+        // }
+        // return Integer.parseInt(res,2);
+        StringBuilder sb=new StringBuilder();
         for(int i=0;i<bin.length();i++){
             char ch=bin.charAt(i);
-            if(ch=='1'){
-                res+='0';
+            if(ch=='0'){
+                sb.append('1');
             }
-            else if(ch=='0'){
-                res+='1';
+            else if(ch=='1'){
+                sb.append('0');
             }
+
         }
-        return Integer.parseInt(res,2);
+        return Integer.parseInt((sb.toString()),2);
     }
 }
