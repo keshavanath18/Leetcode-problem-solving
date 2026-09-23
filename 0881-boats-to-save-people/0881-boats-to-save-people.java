ea3d50f6a4dca7865edae643098f678ccc4c994c
keshavanath18 <keshavav573@gmail.com>
2026-09-23 22:25:54 +0530

    Time: 20 ms (81.48%), Space: 56.1 MB (96.57%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int c=0;
+        Arrays.sort(people);
+        int l=0,r=people.length-1;
+        while(l<=r){
+            if(people[l]+people[r]<=limit){
+                l++;
+            }
+            r--;
+            c++;
+        }
+        return c;
+    }
+}
